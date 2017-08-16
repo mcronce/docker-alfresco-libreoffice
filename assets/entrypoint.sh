96aa7b85c0da513e9d95cdfaa875331ee0022d36
Mike Cronce <mike@quadra-tec.net>
2017-08-15 20:22:57 -0400

    assets/entrypoint.sh:  Run soffice without the .bin extension, apparently that's required now

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -6,8 +6,9 @@ PATH=$LIBREOFFICE_HOME/program:$PATH
 
 if [ "$1" == "run" ]
 then
-	echo soffice.bin --nofirststartwizard --nologo --headless --norestore --invisible  --accept='socket,host=0.0.0.0,port=8100,tcpNoDelay=1;urp'
-	exec soffice.bin --nofirststartwizard --nologo --headless --norestore --invisible  --accept='socket,host=0.0.0.0,port=8100,tcpNoDelay=1;urp'
+	echo soffice --nofirststartwizard --nologo --headless --norestore --invisible  --accept='socket,host=0.0.0.0,port=9100,tcpNoDelay=1;urp'
+	exec soffice --nofirststartwizard --nologo --headless --norestore --invisible  --accept='socket,host=0.0.0.0,port=9100,tcpNoDelay=1;urp'
 else
 	exec "$@"
 fi
+
